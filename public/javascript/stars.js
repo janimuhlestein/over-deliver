102c4a7acd7cf00e4e694e4fe55f90532a48f344
willsan0723 <william.santee@gmail.com>
2020-11-08 19:09:10 -0700

    set up starts of pages for signup/login/search. Fixed logic of stars.js so it outputs only the category when stars are selected.

--- public/javascript/stars.js
+++ public/javascript/stars.js
@@ -1,3 +1,5 @@
 $(':radio').change(function () {
-    console.log(this.closest('h2').textContent.slice(0, -16) + ' star rating: ' + this.value);
+    let str = this.closest('h2').textContent;
+    str = str.replace("&nbsp", "").slice(0, -15).trim().slice(0, -1);
+    console.log(str + ' star rating: ' + this.value);
 });
