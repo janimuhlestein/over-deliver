107ec6147566999d2edd2030340aa8fd7dc0467c
Jonny Lightheart <jonnylightheart@gmail.com>
2020-11-13 22:26:39 -0700

    added technologies to readme.md

--- controllers/searchpage-routes.js
+++ controllers/searchpage-routes.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const { Review, User } = require('../models')
 
 //display search page
-router.get('/search', (req,res)=>{
+router.get('/', (req,res)=>{
     res.render('login', {title: 'Search'}).catch(err=>{
         console.log(err);
         res.status(500).json(err);
