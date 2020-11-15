ff1fb0bc48de6813f8c08ab57a04f07899642c93
Sarah Gambles <sarahgambles@Sarahs-MacBook-Pro.local>
2020-11-15 15:07:37 -0700

    added styling

--- db/schema 2.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE IF EXISTS over_deliver_db;
-
-CREATE DATABASE over_deliver_db;
--- public/javascript/stars 2.js
+++
@@ -1,6 +0,0 @@
-$(':radio').change(function () {
-    let str = this.closest('h2').textContent;
-    str = str.replace("&nbsp", "").slice(0, -15).trim().slice(0, -1).toLowerCase();
-    this.closest('div').id = str;
-    this.closest('div').title = this.value;
-});
